77c68759da6ab75a36506f0f6a50322198aa90c5
benbek <ben@quixey.com>
2016-01-11 17:41:06 +0200

    Removed unneeded fields and methods

--- js/content.js
+++ js/content.js
@@ -3,9 +3,6 @@
 // Extension namespace
 var sn = sn || {};
 
-sn.SHIFT_KEYCODE = 16;
-sn.X_KEYCODE = 88;
-
 sn.getFirstElementByXpath = function(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 };
@@ -15,11 +12,8 @@ sn.getFirstElementByXpath = function(path) {
 
 sn.Bar = function() {
   this.boundHandleRequest_ = this.handleRequest_.bind(this);
-  this.boundMouseMove_ = this.mouseMove_.bind(this);
-  this.boundKeyDown_ = this.keyDown_.bind(this);
 
   this.inDOM_ = false;
-  this.currEl_ = null;
 
   this.barFrame_ = document.createElement('iframe');
   this.barFrame_.src = chrome.runtime.getURL('panel.html');
@@ -27,7 +21,6 @@ sn.Bar = function() {
   // Init to hidden so first showBar_() triggers fade-in.
   this.barFrame_.classList.add('hidden');
 
-  document.addEventListener('keydown', this.boundKeyDown_);
   chrome.runtime.onMessage.addListener(this.boundHandleRequest_);
 };
 
@@ -39,7 +32,6 @@ sn.Bar.prototype.showBar_ = function() {
   var that = this;
   function impl() {
     that.barFrame_.classList.remove('hidden');
-    document.addEventListener('mousemove', that.boundMouseMove_);
   }
   if (!this.inDOM_) {
     this.inDOM_ = true;
@@ -52,7 +44,6 @@ sn.Bar.prototype.hideBar_ = function() {
   var that = this;
   function impl() {
     that.barFrame_.classList.add('hidden');
-    document.removeEventListener('mousemove', that.boundMouseMove_);
   }
   window.setTimeout(impl, 0);
 };
@@ -80,17 +71,6 @@ sn.Bar.prototype.handleRequest_ = function(request, sender, cb) {
     this.hideBar_();
     window.focus();
   } else if (request.type === 'toggleBar') {
-    this.toggleBar_();
-  }
-};
-
-sn.Bar.prototype.mouseMove_ = function(e) {
-};
-
-sn.Bar.prototype.keyDown_ = function(e) {
-  var ctrlKey = e.ctrlKey || e.metaKey;
-  var shiftKey = e.shiftKey;
-  if (e.keyCode === sn.X_KEYCODE && ctrlKey && shiftKey) {
     this.toggleBar_();
   }
 };
